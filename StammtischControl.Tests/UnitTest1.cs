8386465422643bead19b798989a64159ee7a74bf
Ederson <ederson@inventti.com.br>
2016-02-23 07:24:19 -0300

    Criado estrutura de diretorios

--- StammtischControl.Tests/UnitTest1.cs
+++ StammtischControl.Tests/UnitTest1.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace StammtischControl.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
+
         }
     }
 }
